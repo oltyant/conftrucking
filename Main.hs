7587a09569921e294f20ffbd5873474432386737
Tamas Oltyan <oltyant@gmail.com>
2016-09-24 03:04:05 +0200

    [Added parser functions and vanilla unit tests]

--- Main.hs
+++ Main.hs
@@ -1,8 +1,26 @@
+module Main where
+
 import System.Environment
+import Text.Regex.Base
+import Text.Regex.TDFA
 
 main = do
   args <- getArgs
   content <- readFile $ args !! 0
   let inputLines = lines content
   mapM_ (\line -> putStrLn $ line ++ ": written") inputLines
-      
+
+    
+parseLines :: [String] -> [(String, Int)]
+parseLines ss = map splitLine ss
+
+
+splitLine :: String -> (String, Int)
+splitLine s = (title, time)
+  where splitted = words s
+        title    = unwords $ init splitted
+        res      = last splitted =~ "([0-9]+|lightning)" :: String
+        time
+          | res == "lightning" = 5
+          | otherwise          = read res :: Int
+                  
---
+++ Tests.hs
@@ -0,0 +1,19 @@
+module SolutionTest where
+
+import Main (parseLines, splitLine)
+import Test.HUnit
+
+testSplitLineVanilla = TestCase $ assertEqual
+  "Destructure a string to a String and Int tuple"
+  ("And a lazy one", 45)
+  (splitLine "And a lazy one 45min")
+
+testPairOfLines = TestCase $ assertEqual
+  "Every line should contain name, time pairs"
+  [("Create a lightning fast app", 5), ("And a lazy one", 45)]
+  (parseLines ["Create a lightning fast app lightning","And a lazy one 45min"])
+
+tests = TestList [TestLabel "testSplitLineVanilla" testSplitLineVanilla,
+                  TestLabel "testPairOfLines" testPairOfLines]
+
+main = runTestTT $ tests
